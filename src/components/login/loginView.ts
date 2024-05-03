c2c279a0f7a9b719985351669353ab075fd026ce
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-05-03 20:48:20 +0700

    init: start input validation

--- src/components/login/loginView.ts
+++ src/components/login/loginView.ts
@@ -9,13 +9,28 @@ export default class Login {
   }
 
   renderPage() {
-    HTMLCreation.createElement('h1', { class: 'title' }, ['Online-shop']); // For example
-    const a = this.controller; // For example
-    return a;
-  }
+    const loginWrapper = HTMLCreation.createElement('section', { class: 'login__wrapper' }, [
+      HTMLCreation.createElement(
+        'form',
+        {
+          id: 'loginform',
+          class: 'login__form',
+        },
+        [
+          HTMLCreation.createElement('input', { type: 'text', placeholder: 'Username', class: 'login__username' }),
+          HTMLCreation.createElement('input', { type: 'password', placeholder: 'Password', class: 'login__password' }),
+          HTMLCreation.createElement('button', { type: 'submit', value: 'Login', class: 'login__submit-button' }),
+        ]
+      ),
+    ]);
 
-  addEventListeners() {
-    const a = this.controller; // For example
-    return a;
+    const registrationButton = HTMLCreation.createElement('div', { class: 'login__registration-wrapper' }, [
+      HTMLCreation.createElement('button', { class: 'login__registration-button' }, ['Registration']),
+    ]);
+
+    const main = HTMLCreation.createElement('main', {}, [loginWrapper, registrationButton]);
+
+    const body = document.querySelector('body') as HTMLElement;
+    body.append(main);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 import './style.scss';
+import Login from './components/login/loginView';
+
+const a = new Login();
+a.renderPage();
