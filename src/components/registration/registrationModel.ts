ef594a77698026f8bfd5e5805ce03094cb1ccce6
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-05-05 13:11:44 +0200

    feat: RSS-ECOMM-2_11 integrate the registration form with a chosen authentication service

--- src/components/registration/registrationModel.ts
+++ src/components/registration/registrationModel.ts
@@ -1,1 +1,13 @@
-export default class RegistrationModel {}
+import CommerceToolsAPI from '../commerceToolsAPI';
+
+export default class RegistrationModel {
+  async register(email: string, password: string) {
+    try {
+      const commerceToolsAPI = new CommerceToolsAPI();
+      const response = await commerceToolsAPI.register(email, password);
+      return response;
+    } catch (error) {
+      return new Error('Unsuccessful registration');
+    }
+  }
+}
