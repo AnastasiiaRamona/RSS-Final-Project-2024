477a7844eddcb18aac6e8d3211fd439d3dcd0930
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-05-17 10:11:53 +0700

    feat: add test fot class LoginController

---
+++ src/test/loginView.test.ts
@@ -0,0 +1,31 @@
+// import LoginView from '../components/login/loginView';
+// import LoginController from '../components/login/loginController';
+
+// jest.mock('../components/login/loginModel');
+
+// describe('Test class LoginloginView', () => {
+//   const loginView = new LoginView();
+//   let emailInputElement ;
+//   let passwordInputElement ;
+//   let emailErrorElement ;
+//   let passwordErrorElement ;
+//   beforeAll(() => {
+//     (LoginController as jest.Mock).mockImplementation(() => null);
+//   });
+
+//   beforeEach(() => {
+//     document.body.innerHTML = `
+//       <div>
+//         <input class="login__email" type="text" class="login__input">
+//         <input class="login__password-input" type="password" class="login__input">
+//         <div class="login__email-error login__error-hide"></div>
+//         <div class="login__password-error login__error-hide"></div>
+//       </div>
+//     `;
+//   });
+
+//   const emailInputElement = document.querySelector('.login__email');
+//   const passwordInputElement = document.querySelector('.login__password-input');
+//   const emailErrorElement = document.querySelector('.login__email-error');
+//   const passwordErrorElement = document.querySelector('.login__password-error');
+// });
