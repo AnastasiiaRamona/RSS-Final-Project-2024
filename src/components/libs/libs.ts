dc4f7760966e18e65185d3e76dae9fd135dd4618
Oleksandr <aleks6699@gmail.com>
2024-05-23 23:24:34 +0300

    fix: RSS-ECOMM-3_30 update design

--- src/components/libs/libs.ts
+++ src/components/libs/libs.ts
@@ -3,6 +3,7 @@ import { Navigation, Pagination, Zoom } from 'swiper/modules';
 import { Fancybox } from '@fancyapps/ui';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const swiper = new Swiper('.swiper', {
     // configure Swiper to use modules
     modules: [Navigation, Pagination, Zoom],
