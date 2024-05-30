6f37440c23ff5ca8bc78aeba86f3abfc701909a5
Anastasiia Kabanova <118848236+AnastasiiaRamona@users.noreply.github.com>
2024-05-30 11:00:45 +0200

    feat: RSS-ECOMM-3_15 add edit mode design, add API methods for updating of personal info section (#63)
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>

--- src/components/userProfile/userProfileView.ts
+++ src/components/userProfile/userProfileView.ts
@@ -2,9 +2,12 @@ import { Address } from '@commercetools/platform-sdk';
 import iso3166 from 'iso-3166-1-alpha-2';
 import HTMLCreator from '../HTMLCreator';
 import UserProfileController from './userProfileController';
+import editIcon from '../../assets/edit.svg';
 
 export default class UserProfile {
   controller: UserProfileController;
+
+  private version: number | undefined = undefined;
 
   private email: string | undefined = undefined;
 
@@ -32,13 +35,14 @@ export default class UserProfile {
 
   async getCustomerData() {
     const customerData = await this.controller.getCustomerById();
-    if (customerData) {
+    if (customerData && customerData.dateOfBirth) {
       this.email = customerData.email;
       this.password = customerData.password;
       this.firstName = customerData.firstName;
       this.lastName = customerData.lastName;
-      this.dateOfBirth = customerData.dateOfBirth;
+      this.dateOfBirth = this.formatDate(customerData.dateOfBirth);
       this.addresses = customerData.addresses;
+      this.version = customerData.version;
 
       this.billingAddress = this.addresses?.find(
         (address) =>
@@ -47,8 +51,6 @@ export default class UserProfile {
             ? customerData.billingAddressId[0]
             : customerData.billingAddressId)
       );
-
-      console.log(this.billingAddress);
 
       this.shippingAddress = this.addresses?.find(
         (address) =>
@@ -74,24 +76,15 @@ export default class UserProfile {
 
   async renderPage() {
     await this.getCustomerData();
-    if (this.dateOfBirth) {
-      this.dateOfBirth = this.formatDate(this.dateOfBirth);
-    }
+
     const profileContainer = HTMLCreator.createElement('div', { class: 'profile-container' }, []);
 
-    const personalInfoSection = HTMLCreator.createElement('div', { class: 'personal-info-section' }, [
-      HTMLCreator.createElement('h2', {}, ['Personal Information']),
-      HTMLCreator.createElement('p', {}, [`Email: ${this.email}`]),
-      HTMLCreator.createElement('p', {}, [`Password`]),
-      HTMLCreator.createElement('p', {}, [`First Name: ${this.firstName}`]),
-      HTMLCreator.createElement('p', {}, [`Last Name: ${this.lastName}`]),
-      HTMLCreator.createElement('p', {}, [`Date of Birth: ${this.dateOfBirth}`]),
-    ]);
+    const personalInfoSection = this.renderPersonalInfoSection();
 
     const toggleButton = HTMLCreator.createElement('div', { class: 'toggle-button' });
 
     const addressesSection = HTMLCreator.createElement('div', { class: 'addresses-section' }, [
-      HTMLCreator.createElement('h2', {}, ['Saved Addresses', toggleButton]),
+      HTMLCreator.createElement('h2', { class: 'saved-addresses-title' }, ['Saved Addresses', toggleButton]),
       ...(this.addresses || []).map((address) => {
         let addressIcon: HTMLElement | null = null;
         if (this.billingAddress && address.id === this.billingAddress.id) {
@@ -151,6 +144,18 @@ export default class UserProfile {
     return main;
   }
 
+  renderPersonalInfoSection() {
+    const editPersonalButton = this.renderEditButton();
+    const personalInfoSection = HTMLCreator.createElement('div', { class: 'personal-info-section' }, [
+      HTMLCreator.createElement('h2', {}, ['Personal Information', editPersonalButton]),
+      HTMLCreator.createElement('p', {}, [`Email: ${this.email}`]),
+      HTMLCreator.createElement('p', {}, [`First Name: ${this.firstName}`]),
+      HTMLCreator.createElement('p', {}, [`Last Name: ${this.lastName}`]),
+      HTMLCreator.createElement('p', {}, [`Date of Birth: ${this.dateOfBirth}`]),
+    ]);
+    return personalInfoSection;
+  }
+
   formatDate(date: string) {
     const dateObject = new Date(date);
     const month = dateObject.getMonth() + 1 < 10 ? `0${dateObject.getMonth() + 1}` : `${dateObject.getMonth() + 1}`;
@@ -183,15 +188,160 @@ export default class UserProfile {
   addEventListeners() {
     const toggleButton = document.querySelector('.toggle-button');
     let isRotated = false;
+    const addressEntries = document.querySelectorAll('.address-entry');
 
     toggleButton?.addEventListener('click', () => {
       isRotated = !isRotated;
       toggleButton.classList.toggle('rotate', isRotated);
 
-      const addressEntry = document.querySelectorAll('.address-entry');
-      addressEntry.forEach((entry) => {
+      const savedAddressesTitle = document.querySelector('.saved-addresses-title');
+
+      addressEntries.forEach((entry) => {
         entry.classList.toggle('hidden', !isRotated);
       });
+
+      if (isRotated && savedAddressesTitle) {
+        const editButton = this.renderEditButton();
+        // this.addEventListenerToTheEditButton(editButton, addressEntries[0], addressEntries[1]);
+        savedAddressesTitle.appendChild(editButton);
+      } else if (savedAddressesTitle) {
+        const editButton = savedAddressesTitle.querySelector('.edit-button');
+        if (editButton) {
+          savedAddressesTitle.removeChild(editButton);
+        }
+      }
     });
+
+    const editButton = document.querySelector('.edit-button') as HTMLElement;
+    const personalInfoSection = document.querySelector('.personal-info-section') as HTMLElement;
+    if (editButton) {
+      this.addEventListenerToTheEditButton(editButton, personalInfoSection);
+    }
+  }
+
+  renderEditButton() {
+    const editButton = HTMLCreator.createElement('img', { class: 'edit-button', src: editIcon, alt: 'edit button' });
+    return editButton;
+  }
+
+  addEventListenerToTheEditButton(editButton: HTMLElement, container: Element, secondContainer?: Element) {
+    editButton.addEventListener('click', () => {
+      if (editButton.classList.contains('clicked-icon')) {
+        editButton.classList.remove('clicked-icon');
+        const personalInfoSection = container;
+        personalInfoSection.innerHTML = this.renderPersonalInfoSection().innerHTML;
+        this.reassignEvents(container);
+      } else {
+        editButton.classList.add('clicked-icon');
+        this.toggleEditMode(container);
+        if (secondContainer) {
+          this.toggleEditMode(secondContainer);
+        }
+      }
+    });
+  }
+
+  toggleEditMode(element: Element) {
+    const paragraphs = element.querySelectorAll('p');
+    const inputClasses = ['input-mail', 'input-username', 'input-surname', 'input-date'];
+
+    paragraphs.forEach((paragraph: HTMLElement, index: number) => {
+      const paragraphText = paragraph.textContent || '';
+      const splittedText = paragraphText.split(':');
+      const text = splittedText.length > 1 ? splittedText[1].trim() : '';
+
+      const form = HTMLCreator.createElement('form', { class: 'personal-info-form' }) as HTMLFormElement;
+      const input = HTMLCreator.createElement('input', { type: 'text', value: text }) as HTMLInputElement;
+      if (index < inputClasses.length) {
+        input.classList.add(inputClasses[index]);
+      }
+
+      const resetButton = HTMLCreator.createElement('input', {
+        type: 'image',
+        class: 'reset-img',
+        src: 'https://cdn-icons-png.freepik.com/512/2618/2618245.png',
+      });
+
+      const submitButton = HTMLCreator.createElement('input', {
+        class: 'submit-img',
+        type: 'image',
+        src: 'https://icons.iconarchive.com/icons/icons8/windows-8/256/Very-Basic-Ok-icon.png',
+      });
+
+      resetButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        input.value = '';
+        const inputEvent = new Event('input', { bubbles: true });
+        input.dispatchEvent(inputEvent);
+      });
+
+      submitButton.addEventListener('click', async (event) => {
+        event.preventDefault();
+        this.controller.checkValidate(form);
+        if (form.reportValidity()) {
+          const changedParagraph = paragraph;
+          changedParagraph.textContent = input.value;
+          const successTextInput = HTMLCreator.createElement('span', { class: 'success-text-input' }, [
+            'The changes were successfully saved.',
+          ]);
+
+          const id = this.controller.getID();
+          if (id && this.version) {
+            switch (index) {
+              case 0:
+                this.email = input.value;
+                await this.controller.updateEmail(this.version, id, this.email);
+                break;
+              case 1:
+                this.firstName = input.value;
+                await this.controller.updateFirstName(this.version, id, this.firstName);
+                break;
+              case 2:
+                this.lastName = input.value;
+                await this.controller.updateLastName(this.version, id, this.lastName);
+                break;
+              case 3:
+                this.dateOfBirth = input.value;
+                await this.controller.updateDateOfBirth(
+                  this.version,
+                  id,
+                  this.controller.parseDateString(this.dateOfBirth)
+                );
+                break;
+              default:
+                break;
+            }
+            await this.getCustomerData();
+            const existingElement = form.querySelector('.success-text-input');
+
+            if (existingElement) {
+              form.replaceChild(successTextInput, existingElement);
+            } else {
+              form.appendChild(successTextInput);
+            }
+            setTimeout(() => {
+              if (form.contains(successTextInput)) {
+                form.removeChild(successTextInput);
+              }
+            }, 5000);
+          }
+        }
+      });
+
+      form.appendChild(input);
+      form.appendChild(resetButton);
+      form.appendChild(submitButton);
+
+      paragraph.replaceWith(form);
+
+      this.controller.checkValidate(form);
+    });
+  }
+
+  reassignEvents(container: Element) {
+    const newEditButton = container.querySelector('.edit-button') as HTMLElement;
+    if (newEditButton) {
+      this.addEventListenerToTheEditButton(newEditButton, container);
+    }
   }
 }
