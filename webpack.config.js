6bf64fef302dc5ed91fe15e4d487a420e852b4e7
Anastasiia Kabanova <118848236+AnastasiiaRamona@users.noreply.github.com>
2024-06-03 21:16:44 +0200

    Sprint 1: Repository Setup, Project Management, CommerceTools Integration, and Development Environment Configuration (#25)
    * init: RSS-ECOMM-1_01 init the project (#1)
    
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>
    
    * init: RSS-ECOMM-1_02 init organize folder structure (#2)
    
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>
    
    * fix: fix name of components folder
    
    * init: RSS-ECOMM-1_08 сonfigure Webpack
    
    * refactor: RSS-ECOMM-1_08 сonfigure Webpack
    
    * init: RSS-ECOMM-1_09 configure TypeScript
    
    * init: RSS-ECOMM-1_10 set up ESLint
    
    * refactor: RSS-ECOMM-1_10 added a rule in Eslint
    
    * init: RSS-ECOMM-1_11 configure Prettier
    
    * init: RSS-ECOMM-1_12 added pre-commit hook
    
    * init: RSS-ECOMM-1_12 added pre-push  hook
    
    * init: RSS-ECOMM-1_13 install and configure Jest
    
    * feat: RSS-ECOMM-1_14 create ESLint script
    
    * RSS-ECOMM-1_21 fix (#6)
    
    * docs: RSS_ECOMM-1_21 add template of PR
    
    * fix: RSS-ECOMM-1_21 fix the PR-template file configuration
    
    ---------
    
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>
    
    * feat: RSS-ECOMM-1_15 create Prettier script
    
    * feat: RSS-ECOMM-1_16 create Jest test script
    
    * fix: fix configuration of template file
    
    * refactor: RSS-ECOMM-1_09 changed the skipLibCheck setting
    
    * feat: RSS-ECOMM-1_10 add plugins: eslint-plugin-jest and eslint-prettier
    
    * refactor: RSS-ECOMM-1_14 update  script lint
    
    * fix: RSS-ECOMM-1_16 update script
    
    * feat: RSS-ECOMM-1_10 add plugin eslint-config-prettier
    
    * fix: RSS-ECOMM-1_11-fix fix pattern validation of branch name (#31)
    
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>
    
    * docs: add README file from main branch
    
    ---------
    
    Co-authored-by: Anastasia <anastasia@MacBook-Air-Anastasia.local>
    Co-authored-by: Oleksandr <aleks6699@gmail.com>
    Co-authored-by: Oleksandr  Tsurkan <58092216+aleks6699@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|)$/,
+        type: "asset/inline",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+};
