9ef14b5483a51535acd2a49419c697788399ec2b
Oleksandr <aleks6699@gmail.com>
2024-06-12 17:32:34 +0300

    feat: called the toggleAllButtonsToCard method

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -180,6 +180,7 @@ export default class App {
     renderRoute('/catalog', async () => {
       this.changeMainElement(await this.catalog.renderPage());
       this.catalog.addEventListeners();
+      this.catalog.toggleAllButtonsToCard();
       this.appButtonsMethods?.toggleButton(catalogButton, this.buttonsArray);
     });
 
